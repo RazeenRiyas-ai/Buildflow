2ad4a2f0331035d4e48d7a832098d5b97efdcc63
RazeenRiyas-ai <razeenriyas081@gmail.com>
2026-02-05 11:07:42 +0530

    chore: fix next config type error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: any = {
   /* config options here */
   transpilePackages: ['react-map-gl'],
   eslint: {
